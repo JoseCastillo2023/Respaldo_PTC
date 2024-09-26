78eb457550c84af13ad691d83d6774e1f8462635
JoseCastillo2023 <20220235@ricaldone.edu.sv>
2024-09-26 12:07:07 -0600

    HECHOO

--- DbSportDevelopment.sql
+++ DbSportDevelopment.sql
@@ -1,6 +1,6 @@
-DROP DATABASE IF EXISTS sport;
-CREATE DATABASE sport;
-USE sport;
+DROP DATABASE IF EXISTS PanaderiaHPTC;
+CREATE DATABASE PanaderiaHPTC;
+USE PanaderiaHPTC;
 
 CREATE TABLE tb_administradores (
  id_admin INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
@@ -27,16 +27,13 @@ CREATE TABLE tb_clientes (
   fecha_expiracion_codigo DATETIME NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
-CREATE TABLE tb_tallas (
-   id_talla INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
-   nombre VARCHAR(5) NOT NULL
-)ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
 
 CREATE TABLE tb_categorias (
   id_categoria INT(10) AUTO_INCREMENT PRIMARY KEY NOT NULL,
-  nombre VARCHAR(50) NOT NULL,
-  descripcion VARCHAR(250) DEFAULT NULL,
-  imagen VARCHAR(25) NOT NULL
+  nombre_categoria VARCHAR(50) NOT NULL UNIQUE,
+  descripcion_categoria VARCHAR(250) DEFAULT NULL,
+  imagen_categoria VARCHAR(250) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 CREATE TABLE tb_productos(
@@ -63,7 +60,7 @@ CREATE TABLE tb_comentarios (
   estado_comentario  TINYINT(1) NOT NULL
 );
 
-CREATE TABLE tb_detalle_pedidos (
+CREATE TABLE tb_detalles_pedidos (
   id_detalle INT(10) AUTO_INCREMENT PRIMARY KEY NOT NULL,
   id_producto INT(10) UNSIGNED NOT NULL,
   cantidad_producto SMALLINT(6) UNSIGNED NOT NULL,
@@ -75,172 +72,94 @@ CREATE TABLE tb_pedidos (
   id_pedido INT(10) AUTO_INCREMENT PRIMARY KEY NOT NULL,
   id_cliente INT(10) UNSIGNED NOT NULL,
   direccion_pedido VARCHAR(250) NOT NULL,
-  estado_pedido ENUM('Pendiente','EnCamino','Entregado','Cancelado','Historial') NOT NULL,
+  estado_pedido ENUM('Pendiente','En camino','Entregado','Cancelado','Historial') NOT NULL,
   fecha_registro DATE NOT NULL DEFAULT CURRENT_TIMESTAMP()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
-CREATE TABLE tb_colores (
- id_color INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
- nombre VARCHAR(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
-CREATE TABLE tb_marcas (
-   id_marca INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
-   nombre VARCHAR(20) NOT NULL
-)ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+-- Datos de la tabla de Categorias.
+INSERT INTO tb_categorias (nombre_categoria, descripcion_categoria, imagen_categoria) VALUES ('Pasteles', 'Postres grandes en forma circular', 'Pastel.jfif');
+INSERT INTO tb_categorias (nombre_categoria, descripcion_categoria, imagen_categoria) VALUES ('Galletas', 'Galletas con cacao', 'Galletas.png');
+INSERT INTO tb_categorias (nombre_categoria, descripcion_categoria, imagen_categoria) VALUES ('Bolleria', 'Todo tipo de comida relaciona con los panes', 'Bolleria.jpg');
 
-CREATE TABLE tb_deportes (
-   id_deporte INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
-   nombre VARCHAR(20) NOT NULL
-)ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+-- Datos de la tabla de Productos.
+INSERT INTO tb_productos (nombre_producto, descripcion_producto, precio_producto, existencias_producto, imagen_producto, id_categoria, estado_producto) 
+VALUES ('Pastel de chocolate', 'Pastel de chocolate con frutas', 10.50, 100, 'Pastel_chocolate.jpg', 1, 1);
+INSERT INTO tb_productos (nombre_producto, descripcion_producto, precio_producto, existencias_producto, imagen_producto, id_categoria, estado_producto) 
+VALUES ('Pastel de vainilla', 'Pastel de vainilla con frutas', 9.50, 100, 'Pastel_vainilla.jpg', 1, 1);
+INSERT INTO tb_productos (nombre_producto, descripcion_producto, precio_producto, existencias_producto, imagen_producto, id_categoria, estado_producto) 
+VALUES ('Galletas simples', 'Galletas simples con azucar', 2.50, 100, 'Galletas_masa.jfif', 2, 1);
+INSERT INTO tb_productos (nombre_producto, descripcion_producto, precio_producto, existencias_producto, imagen_producto, id_categoria, estado_producto) 
+VALUES ('Galletas de chispas de chocolate', 'Galletas de chispas', 1.50, 100, 'Galletas_chispas.jpg', 2, 1);
+INSERT INTO tb_productos (nombre_producto, descripcion_producto, precio_producto, existencias_producto, imagen_producto, id_categoria, estado_producto) 
+VALUES ('Dona de fresa', 'Dona de fresa rellena de crema', 1.00, 0, 'Dona_fresa.jpg', 3, 1);
+INSERT INTO tb_productos (nombre_producto, descripcion_producto, precio_producto, existencias_producto, imagen_producto, id_categoria, estado_producto) 
+VALUES ('Dona de chocolate', 'Dona de chocolate rellena de crema', 1.00, 100, 'Dona_chocolate.jpg', 3, 1);
 
-CREATE TABLE tb_generos (
-   id_genero INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
-   nombre VARCHAR(50) NOT NULL
-)ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+-- Datos de la tabla de Clientes.
+INSERT INTO tb_clientes (nombre_cliente, apellido_cliente, dui_cliente, correo_cliente, telefono_cliente, direccion_cliente, nacimiento_cliente, clave_cliente, codigo_recuperacion, fecha_expiracion_codigo, fecha_registro)
+VALUES ('Sofía', 'Navas', '17719188-1', 'sofianavas2006@gmail.com', '77778891', 'San Salvador, Apopa', '2000-01-15', 'Elzapato_2001', '456987', '2020-1-31 23:59:59', '2021-1-30');
 
-CREATE TABLE tb_imagenes (
-   id_imagen INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
-   nombre_imagen VARCHAR(25) NOT NULL,
-   id_producto INT
-)ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+INSERT INTO tb_clientes (nombre_cliente, apellido_cliente, dui_cliente, correo_cliente, telefono_cliente, direccion_cliente, nacimiento_cliente, clave_cliente, codigo_recuperacion, fecha_expiracion_codigo, fecha_registro)
+VALUES ('Miguel', 'Angel', '12222221-1', 'elmascapito@gmail.com', '71779988', 'San Salvador, Delgado', '2000-03-22', 'Elzapato_2002', '456989', '2021-12-31 23:59:59', '2022-2-26');
 
-CREATE TABLE tb_tarjetas (
-   id_tarjeta INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
-   tipo_tarjeta ENUM('Visa','MasterCard') NOT NULL,
-   tipo_uso ENUM('Credito','Debito') NOT NULL,
-   numero_tarjeta INT(16) UNSIGNED NOT NULL,
-   nombre_tarjeta VARCHAR(50) NOT NULL,
-   fecha_expiracion INT(5) UNSIGNED NOT NULL,
-   codigo_verificacion INT(5) UNSIGNED NOT NULL,
-   id_cliente INT(10) UNSIGNED NOT NULL
-)ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
-
-INSERT INTO tb_categorias (id_categoria, nombre, imagen, descripcion) VALUES 
-(1, 'Futbol', 'CategoriaFutbol.png', 'Todos los productos relacionados con el fútbol'),
-(2, 'Baloncesto', 'CategoriaBaloncesto.png', 'Todos los productos relacionados con el baloncesto'),
-(3, 'Voleibol', 'CategoriaVoleibol.png', 'Todos los productos relacionados con el voleibol'),
-(4, 'Sneackers', 'CategoriaZapatos.png', 'Todos los productos relacionados con sneackers');
-
-INSERT INTO tb_productos (id_producto, nombre_producto, descripcion_producto, precio_producto, existencias_producto, imagen_producto, id_categoria, estado_producto, id_administrador, fecha_registro) 
+INSERT INTO tb_clientes (nombre_cliente, apellido_cliente, dui_cliente, correo_cliente, telefono_cliente, direccion_cliente, nacimiento_cliente, clave_cliente, codigo_recuperacion, fecha_expiracion_codigo, fecha_registro)
 VALUES 
-(1, 'Balon de Futbol', 'Balon oficial para partidos de futbol.', 29.99, 150, 'img5.png', 1, 1, 1, '2024-05-30'),
-(2, 'Balon de Futbol', 'Balon oficial para partidos de futbol.', 29.99, 150, 'img5.png', 1, 1, 1, '2024-05-30'),
-(3, 'Balon de Futbol', 'Balon oficial para partidos de futbol.', 29.99, 150, 'img5.png', 1, 1, 1, '2024-05-30'),
-(4, 'Balon de Futbol', 'Balon oficial para partidos de futbol.', 29.99, 150, 'img5.png', 1, 1, 1, '2024-05-30'),
-(5, 'Balon de Basquetball', 'Balon oficial para partidos de basquetball.', 34.99, 100, 'img6.png', 2, 1, 1, '2024-05-30'),
-(6, 'Balon de Basquetball', 'Balon oficial para partidos de basquetball.', 34.99, 100, 'img6.png', 2, 1, 1, '2024-05-30'),
-(7, 'Balon de Basquetball', 'Balon oficial para partidos de basquetball.', 34.99, 100, 'img6.png', 2, 1, 1, '2024-05-30'),
-(8, 'Balon de Basquetball', 'Balon oficial para partidos de basquetball.', 34.99, 100, 'img6.png', 2, 1, 1, '2024-05-30'),
-(9, 'Balon de Voleyball', 'Balon oficial para partidos de voleyball.', 24.99, 200, 'img7.png', 3, 1, 1, '2024-05-30'),
-(10, 'Balon de Voleyball', 'Balon oficial para partidos de voleyball.', 24.99, 200, 'img7.png', 3, 1, 1, '2024-05-30'),
-(11, 'Balon de Voleyball', 'Balon oficial para partidos de voleyball.', 24.99, 200, 'img7.png', 3, 1, 1, '2024-05-30'),
-(12, 'Balon de Voleyball', 'Balon oficial para partidos de voleyball.', 24.99, 200, 'img7.png', 3, 1, 1, '2024-05-30'),
-(13, 'Sneackers Deportivos', 'Zapatos deportivos de alta calidad.', 89.99, 50, 'img1.png', 4, 1, 1, '2024-05-30'),
-(14, 'Sneackers Deportivos', 'Zapatos deportivos de alta calidad.', 89.99, 50, 'img2.png', 4, 1, 1, '2024-05-30'),
-(15, 'Sneackers Deportivos', 'Zapatos deportivos de alta calidad.', 89.99, 50, 'img3.png', 4, 1, 1, '2024-05-30'),
-(16, 'Sneackers Deportivos', 'Zapatos deportivos de alta calidad.', 89.99, 50, 'img4.png', 4, 1, 1, '2024-05-30');
+('Jose Alfredo', 'Ramiréz Ochoa', '13333331-1', 'elmasnegro2006@gmail.com', '72593434', 'San Salvador, Nejapa', '2000-07-10', 'Elzapato_2003', '451765', '2024-12-31 23:59:59', '2022-2-26');
 
-INSERT INTO tb_comentarios (id_producto, id_cliente, calificacion_producto, comentario_producto, fecha_valoracion, estado_comentario)
+INSERT INTO tb_clientes (nombre_cliente, apellido_cliente, dui_cliente, correo_cliente, telefono_cliente, direccion_cliente, nacimiento_cliente, clave_cliente, codigo_recuperacion, fecha_expiracion_codigo, fecha_registro)
 VALUES 
-(1, 1, '5', 'Excelente producto, muy satisfecho con la compra.', '2024-08-01', 1),
-(1, 1, '4', 'Buen producto, aunque el envío se retrasó un poco.', '2024-08-02', 1),
-(2, 1, '3', 'El producto está bien, pero esperaba mejor calidad.', '2024-08-03', 1),
-(2, 1, '2', 'No estoy contento con la compra, el producto no cumplió mis expectativas.', '2024-08-04', 1),
-(3, 1, '5', 'Excelente producto, muy satisfecho con la compra.', '2024-08-01', 1),
-(3, 1, '4', 'Buen producto, aunque el envío se retrasó un poco.', '2024-08-02', 1),
-(4, 1, '3', 'El producto está bien, pero esperaba mejor calidad.', '2024-08-03', 1),
-(4, 1, '2', 'No estoy contento con la compra, el producto no cumplió mis expectativas.', '2024-08-04', 1),
-(5, 1, '3', 'El producto está bien, pero esperaba mejor calidad.', '2024-08-03', 1),
-(5, 1, '2', 'No estoy contento con la compra, el producto no cumplió mis expectativas.', '2024-08-04', 1),
-(6, 1, '3', 'El producto está bien, pero esperaba mejor calidad.', '2024-08-03', 1),
-(6, 1, '2', 'No estoy contento con la compra, el producto no cumplió mis expectativas.', '2024-08-04', 1),
-(7, 1, '3', 'El producto está bien, pero esperaba mejor calidad.', '2024-08-03', 1),
-(7, 1, '2', 'No estoy contento con la compra, el producto no cumplió mis expectativas.', '2024-08-04', 1),
-(8, 1, '3', 'El producto está bien, pero esperaba mejor calidad.', '2024-08-03', 1),
-(8, 1, '2', 'No estoy contento con la compra, el producto no cumplió mis expectativas.', '2024-08-04', 1),
-(9, 1, '3', 'El producto está bien, pero esperaba mejor calidad.', '2024-08-03', 1),
-(9, 1, '2', 'No estoy contento con la compra, el producto no cumplió mis expectativas.', '2024-08-04', 1),
-(10, 1, '3', 'El producto está bien, pero esperaba mejor calidad.', '2024-08-03', 1),
-(10, 1, '2', 'No estoy contento con la compra, el producto no cumplió mis expectativas.', '2024-08-04', 1),
-(11, 1, '3', 'El producto está bien, pero esperaba mejor calidad.', '2024-08-03', 1),
-(11, 1, '2', 'No estoy contento con la compra, el producto no cumplió mis expectativas.', '2024-08-04', 1),
-(12, 1, '3', 'El producto está bien, pero esperaba mejor calidad.', '2024-08-03', 1),
-(12, 1, '2', 'No estoy contento con la compra, el producto no cumplió mis expectativas.', '2024-08-04', 1),
-(13, 1, '3', 'El producto está bien, pero esperaba mejor calidad.', '2024-08-03', 1),
-(13, 1, '2', 'No estoy contento con la compra, el producto no cumplió mis expectativas.', '2024-08-04', 1),
-(14, 1, '3', 'El producto está bien, pero esperaba mejor calidad.', '2024-08-03', 1),
-(14, 1, '2', 'No estoy contento con la compra, el producto no cumplió mis expectativas.', '2024-08-04', 1),
-(15, 1, '2', 'No estoy contento con la compra, el producto no cumplió mis expectativas.', '2024-08-04', 1),
-(15, 1, '3', 'El producto está bien, pero esperaba mejor calidad.', '2024-08-03', 1),
-(16, 1, '2', 'No estoy contento con la compra, el producto no cumplió mis expectativas.', '2024-08-04', 1),
-(16, 1, '3', 'El producto está bien, pero esperaba mejor calidad.', '2024-08-03', 1);
+('David', 'Gómez', '14444441-1', 'david.gomez@example.com', '72666666', 'San Salvador, San Salvador', '2000-12-05', 'Elzapato_2004', '265965', '2024-12-31 23:59:59', '2023-4-30');
+
+INSERT INTO tb_clientes (nombre_cliente, apellido_cliente, dui_cliente, correo_cliente, telefono_cliente, direccion_cliente, nacimiento_cliente, clave_cliente, codigo_recuperacion, fecha_expiracion_codigo, fecha_registro)
+VALUES 
+('Lucía', 'Fernández', '15555551-1', 'lucia.fernandez@example.com', '79555555', 'San Salvador, Ilopango', '2000-04-30', 'Elzapato_2005', '200345', '2024-12-31 23:59:59', '2024-1-30');
+
+INSERT INTO tb_clientes (nombre_cliente, apellido_cliente, dui_cliente, correo_cliente, telefono_cliente, direccion_cliente, nacimiento_cliente, clave_cliente, codigo_recuperacion, fecha_expiracion_codigo, fecha_registro)
+VALUES 
+('Jose', 'Castillo', '15555531-1', 'jose.castillo@example.com', '79555535', 'San Salvador, Ilopango', '2000-04-30', 'Elzapato_2006', '200345', '2024-12-31 23:59:59', '2024-1-30');
+
+INSERT INTO tb_clientes (nombre_cliente, apellido_cliente, dui_cliente, correo_cliente, telefono_cliente, direccion_cliente, nacimiento_cliente, clave_cliente, codigo_recuperacion, fecha_expiracion_codigo, fecha_registro, estado_cliente)
+VALUES 
+('Fernando', 'Paniagua', '15555231-1', 'fer.panconagua@example.com', '79334535', 'San Salvador, Apopa', '2000-04-30', 'Elzapato_2007', '200345', '2024-12-31 23:59:59', '2024-3-30', 0);
+
+INSERT INTO tb_clientes (nombre_cliente, apellido_cliente, dui_cliente, correo_cliente, telefono_cliente, direccion_cliente, nacimiento_cliente, clave_cliente, codigo_recuperacion, fecha_expiracion_codigo, fecha_registro, estado_cliente)
+VALUES 
+('Luis', 'Lopez', '15552531-1', 'luis.lopez2006@example.com', '79515535', 'San Salvador, Ilopango', '2000-04-30', 'Elzapato_2008', '200345', '2024-12-31 23:59:59', '2024-4-30', 0);
 
 
-INSERT INTO tb_tallas (nombre) VALUES
-('XS'),
-('S'),
-('M'),
-('L'),
-('XL');
+INSERT INTO tb_comentarios (id_producto, id_cliente, calificacion_producto, comentario_producto)
+VALUES 
+(1, 1, 5, 'Excelente pastel de chocolate muy cremoso.'),
+(2, 1, 4, 'El pastel de vanilla esta muy ricooo.'),
+(3, 1, 5, 'Las galletas son las mejores con su sabor.'),
+(4, 1, 3, 'Las galletas de chocolate esta bien.'),
+(5, 1, 5, 'Las mejores galletas que he probado.');
 
-INSERT INTO tb_colores (nombre) VALUES
-('Rojo'),
-('Azul'),
-('Verde'),
-('Negro'),
-('Blanco');
 
-INSERT INTO tb_marcas (nombre) VALUES
-('Nike'),
-('Adidas'),
-('Puma'),
-('Reebok'),
-('Under Armour');
-
-INSERT INTO tb_deportes (nombre) VALUES
-('Fútbol'),
-('Baloncesto'),
-('Voleibol'),
-('Tenis'),
-('Natación');
-
-INSERT INTO tb_generos (nombre) VALUES
-('Masculino'),
-('Femenino'),
-('Unisex'),
-('Niños'),
-('Niñas');
 
 SELECT * FROM tb_administradores;
 
 SELECT * FROM tb_clientes;
 
-SELECT * FROM tb_tallas;
 
 SELECT * FROM tb_categorias;
 
-SELECT * FROM tb_tarjetas;
 
 SELECT * FROM tb_productos;
 
-SELECT * FROM tb_detalle_pedidos;
+SELECT * FROM tb_detalles_pedidos;
 
 SELECT * FROM tb_pedidos;
 
-SELECT * FROM tb_colores;
-
-SELECT * FROM tb_marcas;
-
-SELECT * FROM tb_deportes;
-
-SELECT * FROM tb_generos;
-
-SELECT * FROM tb_imagenes;
-
-SELECT * FROM tb_detalle_pedidos;
+SELECT * FROM tb_detalles_pedidos;
 
 SELECT * FROM tb_pedidos;
 
 SELECT * FROM tb_comentarios;
+
+
+
+
+
+
